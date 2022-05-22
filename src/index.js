65ba06a614384e2db99ac50edc76ac6f466c2a51
Fozle Rabbi Shafi <frshafi49@gmail.com>
2022-05-23 00:02:16 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import "react-mde/lib/styles/css/react-mde-all.css";
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById("root"))
